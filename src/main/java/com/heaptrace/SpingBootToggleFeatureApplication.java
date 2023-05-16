d799aa69b7d60b792f9a1c85b38fd4073ff849cd
NareshSachin <misalsachin301@gmail.com>
2023-05-16 22:02:53 +0530

    Spring boot togglz feature implemented

---
+++ src/main/java/com/heaptrace/SpingBootToggleFeatureApplication.java
@@ -0,0 +1,13 @@
+package com.heaptrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootToggleFeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootToggleFeatureApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/heaptrace/entity/Product.java
@@ -0,0 +1,16 @@
+package com.heaptrace.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    private int id;
+    private String name;
+    private double price;
+
+
+}
